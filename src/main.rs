8ba57e8ab95af0dceacde984e6279d865cea53ba
Jakob Schwab <j.schwab@mail.de>
2018-07-22 05:57:19 +0200

    Trying out SDL2 backend

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #[macro_use]
 extern crate gfx;
-extern crate gfx_window_glutin;
-extern crate glutin;
+extern crate gfx_window_sdl;
 extern crate rand;
+extern crate sdl2;
 
 use rand::prelude::*;
 
 use gfx::traits::FactoryExt;
 use gfx::Device;
-use glutin::{Event, GlContext, KeyboardInput, WindowEvent};
+use sdl2::event::Event;
+use sdl2::event::WindowEvent;
+use sdl2::keyboard::Keycode;
 
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
@@ -26,26 +28,26 @@ gfx_defines!{
 }
 
 fn main() {
-    let mut events_loop = glutin::EventsLoop::new();
-    let monitor = Some(
-        events_loop
-            .get_available_monitors()
-            .nth(1)
-            .expect("No monitor found"),
-    );
-    // NOTE: Uncomment the following line to switch to windowed mode
-    //let monitor = None;
+    // Init SDL
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    video_subsystem.gl_set_swap_interval(sdl2::video::SwapInterval::Immediate);
 
-    // Create window and drawing context
-    let window_builder = glutin::WindowBuilder::new()
-        .with_title("Pongi".to_string())
-        .with_fullscreen(monitor)
-        .with_dimensions((1280, 720).into());
-    let context = glutin::ContextBuilder::new()
-        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 2)))
-        .with_vsync(true);
-    let (window, mut device, mut factory, main_color, mut main_depth) =
-        gfx_window_glutin::init::<ColorFormat, DepthFormat>(window_builder, context, &events_loop);
+    // Configure OpenGl
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3, 2);
+
+    // Create window and OpenGL context
+    let mut window_builder = video_subsystem.window("Pongi", 1280, 720);
+    window_builder
+        //.resizable()
+        .fullscreen_desktop()
+        .input_grabbed()
+        .position_centered();
+    let (window, _gl_context, mut device, mut factory, main_color, mut main_depth) =
+        gfx_window_sdl::init::<ColorFormat, DepthFormat>(&video_subsystem, window_builder).unwrap();
+    sdl_context.mouse().show_cursor(false);
 
     // Create create command buffer and pipeline state object
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
@@ -79,60 +81,50 @@ fn main() {
     // State variables
     let mut running = true;
     let mut cursor_pos = (0.0, 0.0);
-    let mut window_dimensions: (f32, f32) = (0.0, 0.0);
+    let mut events = sdl_context.event_pump().unwrap();
 
     // Begin mainloop
     while running {
-        events_loop.poll_events(|event| {
-            if let Event::WindowEvent { event, .. } = event {
-                match event {
-                    WindowEvent::CloseRequested => running = false,
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: glutin::ElementState::Pressed,
-                                virtual_keycode: Some(key),
-                                // modifiers,
-                                ..
-                            },
-                        ..
-                    } => {
-                        use glutin::VirtualKeyCode::*;
-                        match key {
-                            Escape => running = false,
-                            _ => (),
-                        }
-                    }
-                    WindowEvent::Resized(new_dim) => {
-                        window.resize(new_dim.to_physical(window.get_hidpi_factor()));
-                        gfx_window_glutin::update_views(
-                            &window,
-                            &mut pipeline_data.out,
-                            &mut main_depth,
-                        );
-                        window_dimensions = (new_dim.width as f32, new_dim.height as f32);
-                    }
-                    WindowEvent::CursorMoved { position, .. } => {
-                        let cursor_x = position.x as f32 / window_dimensions.0;
-                        let cursor_y = position.y as f32 / window_dimensions.1;
-                        cursor_pos = (2.0 * cursor_x - 1.0, -2.0 * cursor_y + 1.0);
-                    }
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. } => running = false,
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    repeat: false,
+                    ..
+                } => match keycode {
+                    Keycode::Escape => running = false,
                     _ => (),
+                },
+                Event::Window {
+                    win_event: WindowEvent::Resized(..),
+                    ..
+                } => {
+                    gfx_window_sdl::update_views(&window, &mut pipeline_data.out, &mut main_depth);
                 }
+                Event::MouseMotion { x, y, .. } => {
+                    let (window_width, window_height) = window.size();
+                    let cursor_x = x as f32 / window_width as f32;
+                    let cursor_y = y as f32 / window_height as f32;
+                    cursor_pos = (2.0 * cursor_x - 1.0, -2.0 * cursor_y + 1.0);
+                }
+                _ => (),
             }
-        });
+        }
 
-        let aspect_ratio = (window_dimensions.0 as f32) / (window_dimensions.1 as f32);
+        // Determine aspect ratio and create vertices for all squares
+        let (window_width, window_height) = window.size();
+        let aspect_ratio = (window_width as f32) / (window_height as f32);
         let (vertices, indices) = render_context.get_vertices_indices(aspect_ratio, cursor_pos);
         let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&vertices, &*indices);
         pipeline_data.vbuf = vertex_buffer;
 
+        // Draw squares and swap framebuffers
         const BACKGROUND_COLOR: [f32; 4] = [0.7, 0.4, 0.2, 1.0];
         encoder.clear(&pipeline_data.out, BACKGROUND_COLOR);
         encoder.draw(&slice, &pipeline_state_object, &pipeline_data);
-
         encoder.flush(&mut device);
-        window.swap_buffers().expect("Failed to swap framebuffers");
+        window.gl_swap_window();
         device.cleanup();
     }
 }
