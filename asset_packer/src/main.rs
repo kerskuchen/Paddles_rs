ef7f3f141fec9bf3ca42a05f75eefb6ed959ee21
Jakob Schwab <j.schwab@mail.de>
2018-08-05 21:00:21 +0200

    Font shadows test

--- asset_packer/src/main.rs
+++ asset_packer/src/main.rs
@@ -16,19 +16,19 @@ fn main() {
         font_data
     };
 
+    let scale = Scale::uniform(8.0);
     let font = Font::from_bytes(&font_data)
         .unwrap_or_else(|error| panic!("Error constructing font: {}", error));
 
-    let scale = Scale::uniform(32.0);
-    let text = "Hello klains lalalala!";
-    let color = (0, 200, 0);
     let metrics = font.v_metrics(scale);
+    let text = "Test gggg";
 
     let glyphs: Vec<_> = font
         .layout(text, scale, point(20.0, 20.0 + metrics.ascent))
         .collect();
 
     let text_height = (metrics.ascent - metrics.descent).ceil() as u32;
+    println!("{}", text_height);
     let text_width = {
         let left = glyphs
             .first()
@@ -42,17 +42,85 @@ fn main() {
     };
 
     let mut image = DynamicImage::new_rgba8(text_width + 40, text_height + 40).to_rgba();
-    for glyph in glyphs {
+    for glyph in &glyphs {
         if let Some(bounding_box) = glyph.pixel_bounding_box() {
             glyph.draw(|x, y, v| {
+                if v > 0.5 {
+                    image.put_pixel(
+                        x + bounding_box.min.x as u32,
+                        y + bounding_box.min.y as u32,
+                        Rgba {
+                            data: [255, 255, 255, 255],
+                        },
+                    )
+                }
+            });
+        }
+    }
+
+    for y in 0..image.height() {
+        for x in 0..image.width() {
+            if (y as i32) - 1 >= 0
+                && image.get_pixel(x, y - 1)[2] != 0
+                && image.get_pixel(x, y)[3] == 0
+            {
                 image.put_pixel(
-                    x + bounding_box.min.x as u32,
-                    y + bounding_box.min.y as u32,
+                    x,
+                    y,
                     Rgba {
-                        data: [color.0, color.1, color.2, (v * 255.0) as u8],
+                        data: [0, 0, 0, 255],
                     },
                 )
-            });
+            }
+            if (x as i32) - 1 >= 0
+                && image.get_pixel(x - 1, y)[2] != 0
+                && image.get_pixel(x, y)[3] == 0
+            {
+                image.put_pixel(
+                    x,
+                    y,
+                    Rgba {
+                        data: [0, 0, 0, 255],
+                    },
+                )
+            }
+            if x + 1 < image.width()
+                && image.get_pixel(x + 1, y)[2] != 0
+                && image.get_pixel(x, y)[3] == 0
+            {
+                image.put_pixel(
+                    x,
+                    y,
+                    Rgba {
+                        data: [0, 0, 0, 255],
+                    },
+                )
+            }
+            if y + 1 < image.height()
+                && image.get_pixel(x, y + 1)[2] != 0
+                && image.get_pixel(x, y)[3] == 0
+            {
+                image.put_pixel(
+                    x,
+                    y,
+                    Rgba {
+                        data: [0, 0, 0, 255],
+                    },
+                )
+            }
+            if (x as i32) - 1 >= 0
+                && (y as i32) - 1 >= 0
+                && image.get_pixel(x - 1, y - 1)[2] != 0
+                && image.get_pixel(x, y)[3] == 0
+            {
+                image.put_pixel(
+                    x,
+                    y,
+                    Rgba {
+                        data: [0, 0, 0, 255],
+                    },
+                )
+            }
         }
     }
 
