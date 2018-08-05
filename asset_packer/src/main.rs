1b9146bd37d8592e8ff88701938e81bbc3fd0545
Jakob Schwab <j.schwab@mail.de>
2018-08-05 21:20:13 +0200

    Better text border drawing

--- asset_packer/src/main.rs
+++ asset_packer/src/main.rs
@@ -28,7 +28,6 @@ fn main() {
         .collect();
 
     let text_height = (metrics.ascent - metrics.descent).ceil() as u32;
-    println!("{}", text_height);
     let text_width = {
         let left = glyphs
             .first()
@@ -41,6 +40,9 @@ fn main() {
         (right - left) as u32
     };
 
+    const COLOR_GLYPH: [u8; 4] = [255, 255, 255, 255];
+    const COLOR_BORDER: [u8; 4] = [0, 0, 0, 255];
+
     let mut image = DynamicImage::new_rgba8(text_width + 40, text_height + 40).to_rgba();
     for glyph in &glyphs {
         if let Some(bounding_box) = glyph.pixel_bounding_box() {
@@ -49,77 +51,30 @@ fn main() {
                     image.put_pixel(
                         x + bounding_box.min.x as u32,
                         y + bounding_box.min.y as u32,
-                        Rgba {
-                            data: [255, 255, 255, 255],
-                        },
+                        Rgba { data: COLOR_GLYPH },
                     )
                 }
             });
         }
     }
 
+    // Create a border around every glyph in the image
     for y in 0..image.height() {
         for x in 0..image.width() {
-            if (y as i32) - 1 >= 0
-                && image.get_pixel(x, y - 1)[2] != 0
-                && image.get_pixel(x, y)[3] == 0
-            {
-                image.put_pixel(
-                    x,
-                    y,
-                    Rgba {
-                        data: [0, 0, 0, 255],
-                    },
-                )
-            }
-            if (x as i32) - 1 >= 0
-                && image.get_pixel(x - 1, y)[2] != 0
-                && image.get_pixel(x, y)[3] == 0
-            {
-                image.put_pixel(
-                    x,
-                    y,
-                    Rgba {
-                        data: [0, 0, 0, 255],
-                    },
-                )
-            }
-            if x + 1 < image.width()
-                && image.get_pixel(x + 1, y)[2] != 0
-                && image.get_pixel(x, y)[3] == 0
-            {
-                image.put_pixel(
-                    x,
-                    y,
-                    Rgba {
-                        data: [0, 0, 0, 255],
-                    },
-                )
-            }
-            if y + 1 < image.height()
-                && image.get_pixel(x, y + 1)[2] != 0
-                && image.get_pixel(x, y)[3] == 0
-            {
-                image.put_pixel(
-                    x,
-                    y,
-                    Rgba {
-                        data: [0, 0, 0, 255],
-                    },
-                )
-            }
-            if (x as i32) - 1 >= 0
-                && (y as i32) - 1 >= 0
-                && image.get_pixel(x - 1, y - 1)[2] != 0
-                && image.get_pixel(x, y)[3] == 0
-            {
-                image.put_pixel(
-                    x,
-                    y,
-                    Rgba {
-                        data: [0, 0, 0, 255],
-                    },
-                )
+            let pixel_val = image.get_pixel(x, y).clone();
+            if pixel_val.data == COLOR_GLYPH {
+                // We landed on a glyph's pixel. We need to paint a border in our neighbouring
+                // pixels that are not themselves part of a glyph
+                let pairs = vec![(-1, 0), (1, 0), (0, -1), (0, 1), (1, 1)];
+                for pair in pairs {
+                    let neighbour_x = (x as i32 + pair.0) as u32;
+                    let neighbour_y = (y as i32 + pair.1) as u32;
+                    let neighbour_pixel_val = image.get_pixel(neighbour_x, neighbour_y).clone();
+
+                    if neighbour_pixel_val.data != COLOR_GLYPH {
+                        image.put_pixel(neighbour_x, neighbour_y, Rgba { data: COLOR_BORDER })
+                    }
+                }
             }
         }
     }
